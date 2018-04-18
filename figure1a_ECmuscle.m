86ea9fab372e07c4115a8905480c339db60777a0
avlant <avlant@hotmail.com>
2018-04-18 10:24:01 +0200

    Show subsystem distribution of mass

--- figure1a_ECmuscle.m
+++ figure1a_ECmuscle.m
@@ -1,10 +1,21 @@
 load('model/reducedModel')
 addpath('src')
 
+%rename subsystems
+%Pyruvate to Ac-Coa towards TCA
+model.subSystems{findIndex(model.rxns, 'HMR_4137')} = 'Tricarboxylic acid cycle and glyoxylate/dicarboxylate metabolism';
+model.subSystems(ismember(model.subSystems, 'Tricarboxylic acid cycle and glyoxylate/dicarboxylate metabolism')) = {'Tricarboxylic acid cycle'};
+model.subSystems(ismember(model.subSystems, 'Alanine, aspartate and glutamate metabolism')) = {'Tricarboxylic acid cycle'};
+
+
+
 model = mapDataToRxns(model, 'data/RxnAndSA.txt');
 
 %Set specific activity to high
 model.specificActivity(findIndex(model.rxns, 'HMR_6921')) = 3;
+
+rxnsWithValues = model.specificActivity>0;
+histogram(log10(model.specificActivity(rxnsWithValues)), 6)
 
 model = addSpecificActivityConstraint(model, 0.35, 0.1*0.5, 60);
 model = addReversedReactions(model);
@@ -62,12 +73,47 @@ plotExchange = {
     };
 
 plotFullSolution(model, growthRates, fullSolution, plotExchange);
+% 
+% figure()
+% %results = runChemostatExperimentYields(model, growthRates, minimalMedia);
+% fullSolution2 = searchForNOMA(model, growthRates, fullSolution);
+% 
+% plotFullSolution(model, growthRates, fullSolution2, objectiveFunction);
+% 
+% %printFluxesValues(model, fullSolution)
 
-figure()
-%results = runChemostatExperimentYields(model, growthRates, minimalMedia);
-fullSolution2 = searchForNOMA(model, growthRates, fullSolution);
+%%
+massConstraintRow = findIndex(model.mets, 'MassConstraint');
+weightValue = model.b(massConstraintRow,2);
+weightRow = 1000 * full(model.S(massConstraintRow,:));
+fluxDistribution = fullSolution(99,:);
 
-plotFullSolution(model, growthRates, fullSolution2, objectiveFunction);
+%Normalize per half glucose
+fluxDistribution = -0.5*fluxDistribution/fluxDistribution(reactionNumbers(2));
+fluxAndMass = fluxDistribution .*weightRow;
+equations = constructEquations(model);
+[subSystem, indx] = sort(model.subSystems);
 
-%printFluxesValues(model, fullSolution)
+for i = 1:length(subSystem)
+   curRxn =  indx(i);
+   rxnName = model.rxns{curRxn};
+   eqnName = equations{curRxn};
+   if fluxAndMass(curRxn)>10^-5
+       fprintf('%s\t%s\t%s\t%2.2f\t%2.2f\n', rxnName, eqnName, subSystem{i}, fluxDistribution(curRxn), fluxAndMass(curRxn))
+   end
+end
+
+
+
+%%
+fprintf('\n')
+allSubs = unique(subSystem);
+for i = 1:length(allSubs)
+    includedRxns = ismember(model.subSystems, allSubs{i});
+    sumOfMass = sum(fluxAndMass(includedRxns));
+    if sumOfMass>10^-5
+       fprintf('%s\t%2.2f\n', allSubs{i},  sumOfMass)
+    end
+end
+
 
