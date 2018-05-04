8a6265d42eda5bd9592c95d8678afd160fc28685
avlant <avlant@hotmail.com>
2018-05-04 13:30:42 +0200

    Change format of experimental data and add experimental km for lactate

--- src2/compareWithSampleData.m
+++ src2/compareWithSampleData.m
@@ -1,4 +1,4 @@
-function compareWithSampleData(model, data, ATPrate, fullSolution, lag)
+function compareWithSampleData(model, dataFolder, ATPrate, fullSolution, lag)
 %Expected baseline ~ 245 ml O2/min
 
 factorForInternalWork = 0;
@@ -22,22 +22,18 @@ ventMod = max(O2mod, CO2mod);
 ventMod = ventilationModel(max(O2mod, CO2mod));%43 Ml per breath
 
 %Collect sample data
-VO2 = load(['sampleData/' data '/VO2.txt']);
-VCO2 = load(['sampleData/' data '/VCO2.txt']);
-RQest = load(['sampleData/' data '/RQ.txt']);
-%HRData = load('sampleData/HRvsO2.txt');
-ventData = load(['sampleData/' data '/VE.txt']);
+% VO2 = load(['sampleData/' data '/VO2.txt']);
+% VCO2 = load(['sampleData/' data '/VCO2.txt']);
+% RQest = load(['sampleData/' data '/RQ.txt']);
+% ventData = load(['sampleData/' data '/VE.txt']);
+data = importdata(['sampleData/' dataFolder '/data.txt']);
+data = data.data;
+Wdata = data(:,1);
+VO2 = data(:,3);
+VCO2 = data(:,4);
+ventData = data(:,5);
+RQest = VCO2./VO2;
 
-%Add internal work data
-VO2(2:end,1) = VO2(2:end,1) + factorForInternalWork;
-VCO2(2:end,1) = VCO2(2:end,1) + factorForInternalWork;
-RQest(2:end,1) = RQest(2:end,1) + factorForInternalWork;
-ventData(2:end,1) = ventData(2:end,1) + factorForInternalWork;
-
-
-%Smoth sample data
-smoothO2 = tsmovavg(VO2,'s',4,1);
-smoothCO2 = tsmovavg(VCO2,'s',4,1);
 
 %Fit polymodel:
 %Hmodel = polyfit(HRData(:,1), HRData(:,2),2);
@@ -46,51 +42,65 @@ smoothCO2 = tsmovavg(VCO2,'s',4,1);
 subplot(2,2,1)
 hold all
 k = 8;
-X = movmean(VO2(:,1),k);
-Y = movmean(VO2(:,2),k);
-E = movstd(VO2(:,2),k);
+X = movmean(Wdata,k);
+Y = movmean(VO2,k);
+E = movstd(VO2,k);
 color1 = [250 50 80]/256;
-plotErrorArea(X, Y, E, color1)  
-
-Y = movmean(VCO2(:,2),k);
-E = movstd(VCO2(:,2),k);
 color2 = [80 50 250]/256;
-plotErrorArea(X, Y, E, color2)  
+plotErrorArea(X, Y, E, color2)
+scatter(Wdata,VO2, 10, 'MarkerFaceColor', color2,'MarkerEdgeColor', color2, 'MarkerFaceAlpha', 0.4, 'MarkerEdgeAlpha', 0.8)
 
-plot(Wmod, CO2mod, 'linewidth', 2, 'color', color2);
-plot(Wmod, O2mod, 'linewidth', 2, 'color', color1);
+plot(Wmod, O2mod, 'k', 'linewidth', 2);
+
+ylabel('ml/min')
+h = findobj(gca,'Type','line');
+
+ylim([0 inf])
+set(gca,'XTick',[])
+ylabel('ml/min')
+
+subplot(2,2,2)
+hold all
+Y = movmean(VCO2,k);
+E = movstd(VCO2,k);
+
+plotErrorArea(X, Y, E, color2)  
+scatter(Wdata,VCO2, 10, 'MarkerFaceColor', color2,'MarkerEdgeColor', color2, 'MarkerFaceAlpha', 0.4, 'MarkerEdgeAlpha', 0.8)
+
+plot(Wmod, CO2mod, 'k', 'linewidth', 2);
 %scatter(VCO2(:,1), VCO2(:,2), 'bo');
 %scatter(VO2(:,1), VO2(:,2), 'ro');
 
 
-xlabel('W')
 ylabel('ml/min')
+set(gca,'XTick',[])
 h = findobj(gca,'Type','line');
-legend(h, 'vO2', 'vCO2', 'location', 'SE')
 ylim([0 inf])
-
-subplot(2,2,2)
-hold all
-Y = movmean(RQest(:,2),k);
-E = movstd(RQest(:,2),k);
-plotErrorArea(X, Y, E, color2)  
-
-plot(Wmod, CO2mod./O2mod, 'linewidth', 2, 'color', color2);
-%scatter(RQest(:,1), RQest(:,2), 'bo');
-xlabel('W')
-ylabel('RQ')
 
 subplot(2,2,3)
 hold all
-Y = movmean(ventData(:,2),k);
-E = movstd(ventData(:,2),k);
+Y = movmean(RQest,k);
+E = movstd(RQest,k);
 plotErrorArea(X, Y, E, color2)  
+scatter(Wdata,RQest, 10, 'MarkerFaceColor', color2,'MarkerEdgeColor', color2, 'MarkerFaceAlpha', 0.4, 'MarkerEdgeAlpha', 0.8)
 
+plot(Wmod, CO2mod./O2mod, 'k', 'linewidth', 2);
 
-plot(Wmod, ventMod, 'linewidth', 2, 'color', color2);
-%scatter(ventData(:,1), ventData(:,2), 'bx');
-xlabel('W')
-ylabel('VE')
+%scatter(RQest(:,1), RQest(:,2), 'bo');
+ylabel('RQ')
+%set(gca,'XTick',[])
+% subplot(2,2,3)
+% hold all
+% Y = movmean(ventData,k);
+% E = movstd(ventData,k);
+% plotErrorArea(X, Y, E, color2)  
+% 
+% 
+% plot(Wmod, ventMod, 'linewidth', 2, 'color', color2);
+% scatter(Wdata, ventData, 10, 'MarkerFaceColor', color2, 'MarkerEdgeColor', 'none', 'MarkerFaceAlpha', 0.5)
+% 
+% xlabel('W')
+% ylabel('VE')
 % 
 % subplot(2,2,4)
 % hold all
@@ -132,12 +142,16 @@ hold all
 mTransp = getTransport(model, {'L-lactate'}, 'sb', 'sm2');
 lactateFlux = fullSolution(:,mTransp(1));
 lactateMod = tsmovavg(lactateFlux,'s',lag,1);
-lactate = load(['sampleData/' data '/la.txt']);
-plot(lactate(:,1), lactate(:,2), 'o--');
-baseLine = min(lactate(:,2));
+lactate = load(['sampleData/' dataFolder '/la.txt']);
+%plot(lactate(:,1), lactate(:,2), 'o--');
+scatter(lactate(:,1), lactate(:,2), 10, 'MarkerFaceColor', color2,'MarkerEdgeColor', color2, 'MarkerFaceAlpha', 0.4, 'MarkerEdgeAlpha', 0.8)
 
-vmax = 6.5;
-km = 5;
+
+baseLine = mean(lactate(1:4,2));
+
+vmax = 9;
+km = 10.73;
+%https://www.sigmaaldrich.com/catalog/product/roche/lldhro?lang=en&region=SE
 
 basalFlux = MM(baseLine, vmax, km);
 
@@ -147,7 +161,8 @@ Slactate = revMM(lactateFlux, vmax, km);
 %lactateConc = lactateConc + baseLine;
 
 ylim([0 inf])
-plot(W, Slactate, '-', 'linewidth', 2);
+plot(W, Slactate, 'k-', 'linewidth', 2);
+
 xlabel('W')
 ylabel('lactate mM')
 
@@ -174,8 +189,8 @@ function plotErrorArea(X, Y, E, color)
 end
 
 function VE = ventilationModel(gasExchange)
-    SGas = revMM(gasExchange', 6400, 6500);
-    VE = MM(SGas, 300, 17000);
+    SGas = revMM(gasExchange', 5000, 8000);
+    VE = MM(SGas, 180, 17000);
 end
 
 
