a964d64ed2c49ae7f5c3a9594474f9405b653f2a
avlant <avlant@hotmail.com>
2018-05-09 15:04:19 +0200

    Rename conected model

--- figure2_connectedmodel.m
+++ figure2_connectedmodel.m
@@ -1,6 +1,6 @@
-load('model/connectedMuscles')
 addpath('src2')
 addpath('sampleData')
+load('model/connectedMuscles')
 model = superModel;
 settings = [];
 
@@ -10,19 +10,22 @@ model = configureSMatrix(model, -10, 'HMR_7638_m3', 'H+[mm3]');
 
 dwMuscle = 20*(1-0.792); %kg muscle
 m1Ratio = 0.55;
-vO2perDryweight = 1.38 * 1.2 * 2.25;
+vO2perDryweight = 1.38 * 1.2 * 2.4;
 complex1Ratio = 33.45/76.41;
 m2Efficency = 0.5;
 vO2max = 11.9;
-maintainance = 4.3+2.4;
-peripheralFA = 0.02;
-FAFactor = 16.55/76.41 * 0.6;
+maintainance = 4.3;
+internalWork = 2.4;
+peripheralFA = 0.01;
+peripheralFAsynth = 0.2;
+FAFactor = 16.55/76.41 * 0.7;
 
 settings.timeSteps = 20;
 settings.pfba = false;
 
-[ATPrate, fullSolution] = setupAndRunSimulation(model, settings, maintainance, dwMuscle, m1Ratio, vO2perDryweight, m2Efficency, complex1Ratio, vO2max, peripheralFA, FAFactor);
+[ATPrate, fullSolution] = setupAndRunSimulation(model, settings, maintainance, internalWork, dwMuscle, m1Ratio, vO2perDryweight, m2Efficency, complex1Ratio, vO2max, peripheralFA, peripheralFAsynth, FAFactor);
 
+%%
 close all
 clf
 compareWithSampleData(model, 'mikael', ATPrate, fullSolution, 1)
