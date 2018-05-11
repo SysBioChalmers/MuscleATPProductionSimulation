71ccd6da79bbc123fc364cfcf93d65fd20ae4078
avlant <avlant@hotmail.com>
2018-05-11 13:34:01 +0200

    Split maintainance in to internal work and maintainance

--- src2/addSpecializedConstraints.m
+++ src2/addSpecializedConstraints.m
@@ -1,8 +1,12 @@
-function model = addSpecializedConstraints(model, ventilation, maintainance, energy)
+function model = addSpecializedConstraints(model, ventilation, maintainance, internalWork, m1Ratio)
 %Allow transport of objective metabolite:
-objRxn = findIndex(model.rxns, 'JoinMuscleATP1');
-model.ub(objRxn) = energy;
-model.lb(objRxn) = 0;
+% objRxn = findIndex(model.rxns, 'JoinMuscleATP1');
+% model.ub(objRxn) = energy;
+% model.lb(objRxn) = 0;
+atpDisipationM1 = findIndex(model.rxns, 'human_ATPMaintainance_m1');
+atpDisipationM2 = findIndex(model.rxns, 'human_ATPMaintainance_m2');
+model.lb(atpDisipationM1) = internalWork * m1Ratio;
+model.lb(atpDisipationM2) = internalWork * (1-m1Ratio);
 
 %Allow maintainance
 objRxn = findIndex(model.rxns, 'Maintainance');
