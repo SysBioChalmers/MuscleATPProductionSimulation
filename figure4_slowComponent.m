5bb70431165a7df2d3d703f4a9f2d8472c586e6e
Avlant Nilsson <avlant@hotmail.com>
2018-08-31 17:05:51 +0200

    Reparameterize slow component model

--- figure4_slowComponent.m
+++ figure4_slowComponent.m
@@ -1,53 +1,59 @@
+clf
 addpath('src2')
 addpath('src3')
 
-muscleMass = 40;
-muscleWater = 0.8;
-blood = 5;
-lactateReservoir= 40*0.8 + 5;
-lactateTreshold = 250;
-wattMax = 350;
-timeSteps = 3;
+muscleMass = 40; %Kg
+muscleWater = 0.8; 
+blood = 5; %L
+lactateReservoir = muscleMass*muscleWater + blood; %l
+lactateTreshold = 240; %W
+maxLactateFlux = 4; %mol/h
 
-timeSpan = [0, 20*60];
-interestingTime = [0 6*60];
-vMax = 9;
+wattMax = 350; %W
+wattSamples = [250 300 350];
+wattSampleVO2 = [3230 3750 4400]; %ml/min
+
+timeSpan = [0, 60/60];
+interestingTime = [0 15/60];
+vMax = 8;
 basalFlux = 1;
 
 
 
 watt = linspace(0,wattMax);
 lactateFlux = 1*(watt>lactateTreshold);
-lactateFlux(lactateFlux==1) = linspace(0,4,sum(lactateFlux));
-lactateConcentration = convertVlacToConc(lactateFlux, basalFlux, vMax);
-basalConcentration = lactateConcentration(1);
+lactateFlux(lactateFlux==1) = linspace(0,maxLactateFlux,sum(lactateFlux));
+lactateConcentration = convertVlacToConc(lactateFlux + basalFlux, 0, vMax);
+basalAmount = lactateReservoir * lactateConcentration(1);
 
-wattSamples = linspace(lactateTreshold, wattMax, timeSteps);
 lactateSamples = interp1(watt,lactateFlux,wattSamples);
+
+
 
 tIn = linspace(timeSpan(1),timeSpan(2));
 lactateResults = zeros(length(wattSamples), length(tIn));
 
 
 for i = 1:length(wattSamples)
-    odParam = @(T,X) lactateOde(T, X, lactateReservoir, basalFlux, lactateSamples(i));
-    [t,yv] = ode23s(odParam, tIn, basalConcentration);
-    lactateResults(i,:) = yv;
+    odParam = @(T,X) lactateOde(T, X, lactateReservoir, basalFlux, vMax, lactateSamples(i));
+    [t,yv] = ode23s(odParam, tIn, basalAmount);
+    lactateResults(i,:) = yv/lactateReservoir;
 end
 
 
-baseVO2 = 500; %ml/min;
 vO2results = zeros(length(wattSamples), length(tIn));
+
 for i = length(wattSamples):-1:1
-    vLactate = mmLactate(lactateResults(i,:));
-    nonLactateVO2 = baseVO2 + wattSamples(i)*10; %ml/min/W
-    additionalVO2 = 1000*molToMl(vLactate * 3/3600); %ml/mol lactate
-    vO2results(i,:) = nonLactateVO2 + additionalVO2;
+    vLactateOut = mmLactate(lactateResults(i,:), vMax)-basalFlux;
+    vLactateIn = lactateSamples(i);
+    netLactate = vLactateIn-vLactateOut;
+    netOxygen = netLactate * 6 * 1.5/30;
+    oxygenDeficit = molToMl(netOxygen); %ml/mol lactate
+    vO2results(i,:) = wattSampleVO2(i) - oxygenDeficit;
 end
 
 
-
-
+%%
 subplot(2,2,1)
 plot(watt, lactateFlux, 'linewidth', 3)
 title('steady state lactate flux')
@@ -55,17 +61,19 @@ xlabel('W')
 ylabel('vLactate')
 
 subplot(2,2,2)
+hold all
 plot(watt, lactateConcentration, 'linewidth', 3)
 title('steady state lactate concentration')
 xlabel('W')
-ylabel('[lactate]')
+ylabel('lactate concentration [mM]')
+%plot(wattSamples, lactateResults(:,end), 'o')
 
 subplot(2,2,3)
 hold all
 
 wattLabels = [];
 for i = size(lactateResults,1):-1:1
-    plot(tIn, lactateResults(i,:), 'linewidth', 3)
+    plot(60*tIn, lactateResults(i,:), 'linewidth', 3)
     wattLabels{i} = sprintf('%2.0f Watt', wattSamples(i));    
 end
 wattLabels = wattLabels(end:-1:1);
@@ -73,29 +81,32 @@ legend(wattLabels)
 legend boxoff
 
 for i = 1:size(lactateResults,1)
-    plot(interestingTime, (lactateResults(i,end)*[1 1]), 'k--')
+    plot(60*interestingTime, (lactateResults(i,end)*[1 1]), 'k-')
 end
 
-xlabel('time [s]')
-ylabel('[lactate]')
+xlabel('time [min]')
+ylabel('lactate concentration [mmol/l]')
 title('Lactate dynamics')
-xlim(interestingTime)
+xlim(60*interestingTime)
+
+% plot(60*6 * [1 1], lactateResults(i,end)*[0 1], 'k--')
 
 subplot(2,2,4)
 hold all
 for i = size(vO2results,1):-1:1
-    plot(tIn, vO2results(i,:), 'linewidth', 3)
+    plot(60*tIn, vO2results(i,:), 'linewidth', 3)
 end
 
-legend(wattLabels, 'location', 'se')
-legend boxoff
+% legend(wattLabels, 'location', 'se')
+% legend boxoff
 
 for i = 1:size(vO2results,1)
-    plot(interestingTime, vO2results(i,end)*[1 1], 'k--')
+    plot(60*interestingTime, vO2results(i,end)*[1 1], 'k-')
 end
+% plot(60*6 * [1 1], vO2results(i,end)*[0.7 1], 'k--')
 
-
-xlim(interestingTime)
-xlabel('time [s]')
-ylabel('vO2')
+xlim(60*interestingTime)
+xlabel('time [min]')
+ylabel('vO2 [ml/min]')
 title('Oxygen dynamics')
+
