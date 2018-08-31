859b9d5e312199a083905d739fae6c749a0d2caa
Avlant Nilsson <avlant@hotmail.com>
2018-08-31 16:46:49 +0200

    corrected the function name

--- src2/molToMl.m
+++ src2/molToMl.m
@@ -1,5 +1,5 @@
-function result = molToW(data)
-    %Converts mMol/h to ml/min
+function result = molToMl(data)
+    %Converts mol/h to ml/min
     %1 ?mol O2 = .022391 ml
     result = (0.022391 * 10^6)*data;
     result = result / 60;
