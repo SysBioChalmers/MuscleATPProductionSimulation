6cb74393dd4f3db176dc2ad37403ff63526421cf
Avlant Nilsson <avlant@hotmail.com>
2019-03-12 10:47:39 +0100

    rename variable

--- src2/getATPrates.m
+++ src2/getATPrates.m
@@ -1,8 +1,8 @@
 function atpRates = getATPrates(model, settings)
-    maximumSolution = maximizeATP(model, settings);
-    maximumSolution = maximumSolution(findIndex(model.rxns, settings.primaryObjective));
-    if maximumSolution>(10^-6)
-        atpRates = linspace(0, maximumSolution, settings.timeSteps);
+    maxSol = maximizeATP(model, settings);
+    maxSol = maxSol(findIndex(model.rxns, settings.primaryObjective));
+    if maxSol>(10^-6)
+        atpRates = linspace(0, maxSol, settings.timeSteps);
     else
         atpRates = [];
     end
