81de5e767aecb47051af8d6821c786aa220e0033
Avlant Nilsson <avlant@hotmail.com>
2019-03-12 10:53:02 +0100

    change to param object

--- src2/addInternalConstraints.m
+++ src2/addInternalConstraints.m
@@ -1,4 +1,4 @@
-function model = addInternalConstraints(model, dwMuscle, muscleRatio, vO2perDryweight, m2Efficency, complex1Ratio, FAratio, type1tresh, type2target)    
+function model = addInternalConstraints(model, param)    
 
     model = configureSMatrix(model, -1, 'ATPworkOut_m1', 'ATPwork[s]');
     model = configureSMatrix(model, -1, 'ATPworkOut_m2', 'ATPwork[s]');   
@@ -7,32 +7,29 @@ function model = addInternalConstraints(model, dwMuscle, muscleRatio, vO2perDryw
    
     model = configureSMatrix(model, 0, 'JoinMuscleATP1', 'ATPwork[sm1]');
     model = configureSMatrix(model, 0, 'JoinMuscleATP1', 'ATPwork[sm2]'); 
-    model = configureSMatrix(model, 1-type2target, 'JoinMuscleATP1', 'ATPwork[cm1]');
-    model = configureSMatrix(model, type2target, 'JoinMuscleATP1', 'ATPwork[cm2]');     
+    model = configureSMatrix(model, 1-param.type2target, 'JoinMuscleATP1', 'ATPwork[cm1]');
+    model = configureSMatrix(model, param.type2target, 'JoinMuscleATP1', 'ATPwork[cm2]');     
     
-    model.ub(findIndex(model.rxns, 'ATPworkOut_m1')) = type1tresh;
+    model.ub(findIndex(model.rxns, 'ATPworkOut_m1')) = param.type1tresh;
     model.ub(findIndex(model.rxns, 'ATPworkOut_m2')) = 0;
     
-    scaledC1 = dwMuscle*vO2perDryweight*muscleRatio;
-    scaledC2 = dwMuscle*vO2perDryweight*(1-muscleRatio) * m2Efficency;
+    scaledM1 = param.dwMuscle  * param.scalingFactor * param.muscleRatio;
+    scaledM2 = param.dwMuscle  * param.scalingFactor * (1-param.muscleRatio) * param.m2Efficency;
     
+    %Specific activity Complex I
+    model.ub(findIndex(model.rxns,'HMR_6921_m1')) = param.HMR_6921 * scaledM1;
+    model.ub(findIndex(model.rxns,'HMR_6921_m2')) = param.HMR_6921 * scaledM2;
 
-    %the vO2 on complex I substrate vs (I + II)
-    %the stochiometric relation to complex IV
-    cmpxIfactor = 5/3 * complex1Ratio; 
-    
-    fattyAcidFactor = FAratio * 1/23; %stochiometery for fatty acids
-    
-    model.ub(findIndex(model.rxns, 'HMR_6914_m1')) = scaledC1; %Oxygen in m1
-    model.ub(findIndex(model.rxns, 'HMR_6914_m2')) = scaledC2; %Oxygen in m2
-   
-    %complex I
-    model.ub(findIndex(model.rxns, 'HMR_6921_m1')) = cmpxIfactor * scaledC1; %Cplx IV m2x
-    model.ub(findIndex(model.rxns, 'HMR_6921_m2')) = cmpxIfactor * scaledC2; %Cplx IV m2x
-    
-    model.lb(findIndex(model.rxns, 'HMR_0217_m1')) = -fattyAcidFactor * scaledC1; %fatty acid metabolism
-    model.lb(findIndex(model.rxns, 'HMR_0217_m2')) = -fattyAcidFactor * scaledC2; %fatty acid metabolism  
-   
+    %Specific activity Complex IV
+    model.ub(findIndex(model.rxns,'HMR_6914_m1')) = param.HMR_6914 * scaledM1;
+    model.ub(findIndex(model.rxns,'HMR_6914_m2')) = param.HMR_6914 * scaledM2;
+
+    %Specific activity ETF
+    model.ub(findIndex(model.rxns,'HMR_6911_m1')) = param.HMR_6911 * scaledM1;
+    model.ub(findIndex(model.rxns,'HMR_6911_m2')) = param.HMR_6911 * scaledM2;
+
+    %Lactate accumulation flux for non steady state
+    model.ub(findIndex(model.rxns, 'lactateBuffering')) = param.lactateBuffering;
     
 end
 
