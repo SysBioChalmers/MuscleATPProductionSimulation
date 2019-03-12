85fa07539af8b480146544636eacd3e7f698fbd3
Avlant Nilsson <avlant@hotmail.com>
2019-03-12 10:44:10 +0100

    adjust O2 to RER

---
+++ src2/normalizeO2.m
@@ -0,0 +1,13 @@
+function O2 = normalizeO2(vO2, vCO2)
+    RQ = vCO2./vO2;
+    RQ(RQ>1) = 1;
+    RQ(RQ<0.7) = 0.7;
+    
+    vier = @(VO2,RQ) VO2 .* ((((RQ-0.71)./0.29).*21.4)+(((1-RQ)./0.29).*19.4))/60;
+    revvier = @(EE,RQ) EE ./ (((((RQ-0.71)./0.29).*21.4)+(((1-RQ)./0.29).*19.4))/60);
+    
+    EE = vier(vO2, RQ);
+    O2 = revvier(EE, 1);
+
+end
+
