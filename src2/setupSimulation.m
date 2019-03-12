a3961b009f139ca903661b21eeaca427cb439e18
Avlant Nilsson <avlant@hotmail.com>
2019-03-12 10:39:55 +0100

    change input to an object (param)

--- src2/setupSimulation.m
+++ src2/setupSimulation.m
@@ -1,16 +1,10 @@
-function model = setupSimulation(model, maintainance, internalWork, dwMuscle, m1Ratio, vO2perDryweight, m2Efficency, complex1Ratio, vO2max, peripheralFA, peripheralLactateCapacity, FAFactor, type1tresh, type2target)
-    model = addInternalConstraints(model, dwMuscle, m1Ratio, vO2perDryweight, m2Efficency, complex1Ratio, FAFactor, type1tresh, type2target);
-    model = addTransportConstraints(model,   's', {'O2'}, [-vO2max], [0]);
-    model = addTransportConstraints(model, 'sm3', {'palmitate', 'L-lactate'}, [-peripheralFA -peripheralLactateCapacity],  [0 0]);    
+function model = setupSimulation(model, param)
+    model = addInternalConstraints(model, param);
+    model = addTransportConstraints(model,   's', {'O2'}, -param.vO2max, 0);
+    model = addTransportConstraints(model, 'sm3', {'palmitate', 'L-lactate'}, [-param.peripheralFA -param.peripheralLactateCapacity],  [0 0]);    
     model = addTransportConstraints(model, 'sm1', {'palmitate','L-lactate'}, [-1000, -1000],  [0 0]);   
     model = addTransportConstraints(model, 'sm2', {'palmitate','L-lactate'}, [-1000 0], [0 1000]);
-    model = addMaintenanceConstraints(model, 1000, maintainance, internalWork);
-    
-    %Allow production of stearate
-    mTransp = getTransport(model, {'stearate'}, 's', 'sb');
-    model.ub(mTransp) = 1000;
-    mTransp = getTransport(model, {'stearate'}, 'sm3', 'sb');
-    model.ub(mTransp) = 1000;
+    model = addMaintenanceConstraints(model, 1000, param.maintainance, param.internalWork);
     
     model = addExchangeMedum(model);
 
