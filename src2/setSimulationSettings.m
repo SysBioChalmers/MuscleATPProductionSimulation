9fd8f02214a99db9cd77a1e334478ea7534a3895
Avlant Nilsson <avlant@hotmail.com>
2019-03-12 10:40:32 +0100

    add lactate buffering

--- src2/setSimulationSettings.m
+++ src2/setSimulationSettings.m
@@ -4,9 +4,13 @@ function settings = setSimulationSettings(timeSteps, pFBA)
     settings.pfba = pFBA;
     %objective function
     settings.primaryObjective = 'MuscleATPOut';    
-%     settings.minRxns = {'glycogenUsage', 'O2_Exchange_b',  'HMR_9135_m1' 'HMR_9135_m3', 'L-lactate_Exchange_'};
-%     settings.minVal = [0, 1, 0, 0, 5];
-    settings.minRxns = {'O2_Exchange_', 'glycogen_Exchange_', 'L-lactate_Exchange_', 'ATPworkOut_m1', 'HMR_9135_m1', 'HMR_9135_m3'};
-    settings.minVal = [1, 1, -4, 0.01, 1, 3];
+%   settings.minRxns = {'glycogen_Exchange_b', 'O2_Exchange_b',  'HMR_9135_m1' 'HMR_9135_m3', 'L-lactate_Exchange_'};
+%   settings.minVal = [0, 1, 0, 0, 5];
+%   settings.minRxns = {'CO2_Exchange_b', 'ATPworkOut_m1'};
+%   settings.minVal = [-1, 0.01];    
+
+    settings.minRxns = {'O2_Exchange_b', 'glycogen_Exchange_b', 'glucose_Exchange_b', 'lactateBuffering', 'ATPworkOut_m1', 'HMR_9135_m1', 'HMR_9135_m3'};
+    settings.minVal = [1, 1, 1, -3, 0.01, 0.9, 3];
+    
 end
 
