fe0cb1a8c0f3646a37369cfdc6f1bb9155b61550
Avlant <avlant@hotmail.com>
2019-07-21 17:08:29 -0400

    remove newlines

--- src1/configureModel.m
+++ src1/configureModel.m
@@ -2,8 +2,6 @@ function model = configureModel(model, minimalMedia, minimalFlux)
 maintenance = 0;
 
 
-
-
 [exchangeRxns, exchangeRxnsIndexes]=getExchangeRxns(model,'both');
 
 %Minimal media simulation
@@ -16,8 +14,6 @@ model = setParam(model, 'ub', exchangeRxnsIndexes, 1000);
 
 model = setParam(model, 'lb', reactionNumbers, minimalFlux);
 model = setParam(model, 'ub', reactionNumbers, 1000);
-
-
 
 
 model = setParam(model, 'lb', 'human_ATPMaintainance', maintenance);
