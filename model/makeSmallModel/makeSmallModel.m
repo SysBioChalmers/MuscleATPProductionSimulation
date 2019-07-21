5b1e58e313754366e85c56fed0f65af6b20f6aa7
Avlant <avlant@hotmail.com>
2019-07-21 17:07:16 -0400

    Expand model to include palmitate

--- model/makeSmallModel/makeSmallModel.m
+++ model/makeSmallModel/makeSmallModel.m
@@ -7,6 +7,7 @@ ATPProduction = findIndex(model.rxns, objectiveFunction);
 
 %Minimal media simulation
 inMedia = {
+    'palmitate[s]'
     'octanoic acid[s]'
     'glycogen[s]'
     'O2[s]'  
@@ -18,7 +19,6 @@ outMedia = {
     'H2O[s]'
     'L-lactate[s]'
 };
-
 
 
 [exchangeRxns, exchangeRxnsIndexes]=getExchangeRxns(model,'both');
@@ -34,6 +34,8 @@ model = setParam(model, 'ub', reactionOut, 1000);
 model = setParam(model, 'lb', reactionIn, -1000);
 model = setParam(model, 'lb', reactionIn(1), 0);
 model = setParam(model, 'lb', reactionIn(2), 0);
+model = setParam(model, 'lb', reactionIn(3), 0);
+
 
 model = setParam(model, 'lb', objectiveFunction, 0);
 model = setParam(model, 'ub', objectiveFunction, 1000);
@@ -63,7 +65,6 @@ model.subSystems{findIndex(model.rxns, 'HMR_4137')} = 'Tricarboxylic acid cycle 
 model.subSystems(ismember(model.subSystems, 'Tricarboxylic acid cycle and glyoxylate/dicarboxylate metabolism')) = {'Tricarboxylic acid cycle'};
 
 
-
 %Remove all compartments appart from cytosol, mitochondria and extracell
 model.lb(ismember(model.rxnComps, [2 5 6 7 8])) = 0;
 model.ub(ismember(model.rxnComps, [2 5 6 7 8])) = 0;
@@ -72,66 +73,63 @@ model.ub(ismember(model.rxnComps, [2 5 6 7 8])) = 0;
 % model.lb(findIndex(model.rxns,'HMR_4957')) = 0;
 % model.ub(findIndex(model.rxns,'HMR_4957')) = 0;
 
+nrOfCases = 7;
+fluxDistributions = zeros(length(model.rxns), nrOfCases);
 
+for i = 1:nrOfCases
+    tmpModel = model;
+    switch i
+        case 1
+            %Fatty acid flux
+            tmpModel = setParam(tmpModel, 'lb', reactionIn(1), -1);
+        case 2
+            %Fatty acid flux (octanoic acid)
+            tmpModel = setParam(tmpModel, 'lb', reactionIn(2), -1);
+        case 3
+            %Canonical flux
+            tmpModel = setParam(tmpModel, 'lb', reactionIn(3), -1);
+        case 4
+            %Complex 1 knock out
+            tmpModel = setParam(tmpModel, 'lb', reactionIn(3), -1);            
+            tmpModel = setParam(tmpModel, 'lb', 'HMR_6921', 0);
+            tmpModel = setParam(tmpModel, 'ub', 'HMR_6921', 0);
+        case 5
+            %glycerol-3-phosphate shunt knock
+            tmpModel = setParam(tmpModel, 'lb', reactionIn(3), -1);            
+            tmpModel = setParam(tmpModel, 'lb', 'HMR_0483', 0);
+            tmpModel = setParam(tmpModel, 'ub', 'HMR_0483', 0);
+        case 6
+            %ATP synthase KO
+            tmpModel = setParam(tmpModel, 'lb', reactionIn(3), -1);            
+            tmpModel = setParam(tmpModel, 'lb', 'HMR_6916', 0);
+            tmpModel = setParam(tmpModel, 'ub', 'HMR_6916', 0);         
+        case 7
+            %oxygen transport
+            tmpModel = setParam(tmpModel, 'lb', reactionIn(3), -1);            
+            tmpModel = setParam(tmpModel, 'lb', 'HMR_9048', 0);
+            tmpModel = setParam(tmpModel, 'ub', 'HMR_9048', 0);
+        %case 8
+            %ETF-ubiquinone oxidoreductase knock
+            % tmpModel = setParam(tmpModel, 'lb', 'HMR_6911', 0);
+            % tmpModel = setParam(tmpModel, 'ub', 'HMR_6911', 0);            
+            
+    end
+    solution = solveLinMin(tmpModel, true);
+    solution
+    fluxDistributions(:, i) = solution.x;
+    
+end
+totalSolution =  sum(abs(fluxDistributions),2);
 
-
-%Fatty acid flux
-model = setParam(model, 'lb', reactionIn(1), -1);
-solution0 = solveLinMin(model, true)
-
-
-%Canonical flux
-model = setParam(model, 'lb', reactionIn(1), 0);
-model = setParam(model, 'lb', reactionIn(2), -1);
-solutionA = solveLinMin(model, true)
-
-printAffected(model, solutionA, solution0)
-
-%Complex 1 knock out
-model1 = setParam(model, 'lb', 'HMR_6921', 0);
-model1 = setParam(model1, 'ub', 'HMR_6921', 0);
-
-solutionB = solveLinMin(model1, true)
-%printAffected(model, solutionA, solutionB);
-
-
-%glycerol-3-phosphate shunt knock
-model2 = setParam(model1, 'lb', 'HMR_0483', 0);
-model2 = setParam(model2, 'ub', 'HMR_0483', 0);
-solutionC = solveLinMin(model2, true)
-%printAffected(model, solutionB, solutionC)
-
-
-
-
-%ETF-ubiquinone oxidoreductase knock
-% model = setParam(model, 'lb', 'HMR_6911', 0);
-% model = setParam(model, 'ub', 'HMR_6911', 0);
-
-% solutionD = solveLinMin(model, true)
-% printAffected(model, solutionC, solutionD)
-
-%ATP synthase KO
-model3 = setParam(model, 'lb', 'HMR_6916', 0);
-model3 = setParam(model3, 'ub', 'HMR_6916', 0);
-
-solutionD = solveLinMin(model3, true)
-%printAffected(model, solutionC, solutionD)
-
-%oxygen transport
-model4 = setParam(model, 'lb', 'HMR_9048', 0);
-model4 = setParam(model4, 'ub', 'HMR_9048', 0);
-solutionE = solveLinMin(model4, true)
-
-totalSolution = abs(solution0.x) + abs(solutionA.x) + abs(solutionB.x) + abs(solutionC.x) + abs(solutionD.x) + abs(solutionE.x);
+%printAffected(model, solutionA, solutionB)
 
 % clc
 % printExchangeFluxes(model, solutionE.x)
 tresh = 10^-5;
-reducedModel = removeRxns(model,totalSolution<tresh,true,true,true);
+reducedModel = removeRxns(model, totalSolution<tresh, true, true, true);
 
 model = reducedModel;
-save('reducedModel.mat', 'model');
+save('../reducedModel.mat', 'model');
 
 reducedModel = rmfield(reducedModel,'rxnComps');
 reducedModel = rmfield(reducedModel,'geneComps');
